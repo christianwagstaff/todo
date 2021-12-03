17ec867bd8a4cdc2859d3035f49e94785f1add11
Christian <chris23681@gmail.com>
2021-12-02 17:43:43 -0800

    Fixed NPM Build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
@@ -17,7 +17,7 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: '/',
+        // publicPath: '/',
     },
     module: {
         rules: [
